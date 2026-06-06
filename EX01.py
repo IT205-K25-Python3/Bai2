e32e0466f8385319186f70921dc3bfdb453bd55d
Qu04nh <quocanh.04.007@gmail.com>
2026-06-06 15:32:29 +0700

    b2 solved

--- EX01.py
+++ EX01.py
@@ -1,6 +1,6 @@
-ten = input("Nhap ten nhan vien: ")
-so_gio = float(input("Nhap so gio lam viec: "))
-luong_gio = float(input("Nhap luong moi gio: "))
+name = input("Nhap ten nhan vien: ")
+hour = float(input("Nhap so gio lam viec: "))
+hour_salary = float(input("Nhap luong moi gio: "))
 
-luong_thang = so_gio * luong_gio
-print(f"Luong thang cua nhan vien {ten} la: {luong_thang} dong") 
+month_salary = hour * hour_salary
+print(f"Luong thang cua nhan vien {name} la: {month_salary} dong") 
