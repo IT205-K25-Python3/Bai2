cc19bdfa9bff77d668a76e8b1d0a799a5015f4cb
Qu04nh <quocanh.04.007@gmail.com>
2026-06-06 16:13:19 +0700

    b2 solved

--- EX08.py
+++ EX08.py
@@ -1,15 +1,15 @@
-so_du = 10000000
+balance = 10000000
 
 while True:
-    so_tien_rut = float(input("Nhap so tien muon rut (or 0 de thoat): "))
+    withdrawal_amount = float(input("Nhap so tien muon rut (or 0 de thoat): "))
     
-    if so_tien_rut == 0:
+    if withdrawal_amount == 0:
         break
     
-    if so_tien_rut > so_du:
+    if withdrawal_amount > balance:
         print("So tien vut qua so du!")
-    elif so_tien_rut % 50000 != 0:
+    elif withdrawal_amount % 50000 != 0:
         print("So tien phai chia het cho 50000!")
     else:
-        so_du -= so_tien_rut
-        print(f"Rut thanh cong! So du con lai: {so_du}")
+        balance -= withdrawal_amount
+        print(f"Rut thanh cong! So du con lai: {balance}")
