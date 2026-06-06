0ee7690266ba1820d3a4fb66ec1304271ce920c2
Qu04nh <quocanh.04.007@gmail.com>
2026-06-06 15:26:46 +0700

    b2 solved

--- EX11.py
+++ EX11.py
@@ -2,5 +2,5 @@ a = float(input("Nhap a: "))
 b = float(input("Nhap b: "))
 c = float(input("Nhap c: "))
 
-so_lon_nhat = max(a, b, c)
-print(f"So lon nhat la: {so_lon_nhat}")
+max_num = max(a, b, c)
+print(f"So lon nhat la: {max_num}")
