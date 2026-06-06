966971bc3ba7664682a475da5081674345e5ca40
Qu04nh <quocanh.04.007@gmail.com>
2026-06-06 15:22:07 +0700

    Bai2 solved

--- EX05.py
+++ EX05.py
@@ -1,8 +1,8 @@
 n = int(input("Nhap so nguyen duong n: "))
 
-dem = 0
+count = 0
 for i in range(1, n + 1):
     if i % 3 == 0:
-        dem += 1
+        count += 1
 
-print(f"So luong so chia het cho 3 tu 1 den {n} la: {dem}")
+print(f"So luong so chia het cho 3 tu 1 den {n} la: {count}")
