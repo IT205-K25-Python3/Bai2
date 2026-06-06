c8251e5941963394470c11ce0fd97f3e05d38c71
Qu04nh <quocanh.04.007@gmail.com>
2026-06-06 16:11:28 +0700

    b2 solved

--- EX14.py
+++ EX14.py
@@ -3,13 +3,13 @@ km = float(input("Nhap so km khach di: "))
 if km <= 0:
     print("So km phai lon hon 0!")
 else:
-    tong_tien = 0
+    total_cost = 0
     
     if km <= 1:
-        tong_tien = 15000
+        total_cost = 15000
     elif km <= 10:
-        tong_tien = 15000 + (km - 1) * 12000
+        total_cost = 15000 + (km - 1) * 12000
     else:
-        tong_tien = 15000 + 9 * 12000 + (km - 10) * 10000
+        total_cost = 15000 + 9 * 12000 + (km - 10) * 10000
     
-    print(f"Tong tien phai tra: {tong_tien} dong")
+    print(f"Tong tien phai tra: {total_cost} dong")
