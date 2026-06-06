ca7762dbd771ecef995c7da361cee60dd77a62a4
Qu04nh <quocanh.04.007@gmail.com>
2026-06-06 15:25:56 +0700

    b2 solved

--- EX13.py
+++ EX13.py
@@ -1,10 +1,10 @@
-so_bi_mat = 7
+secret_num = 7
 print("Doan so bi mat (7)")
 
 while True:
     doan = int(input("Nhap so: "))
     
-    if doan == so_bi_mat:
+    if doan == secret_num:
         print("Chuc mung! Ban da doan dung!")
         break
     else:
